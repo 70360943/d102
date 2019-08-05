4dc04f5f3d810bf273e1e379a7aaee0742a14153
70360943 <70360943@qq.com>
2019-08-05 15:54:23 +0800

    第一次提交

---
+++ src/main/java/org/java/Git101Application.java
@@ -0,0 +1,13 @@
+package org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git101Application.class, args);
+    }
+
+}
---
+++ src/main/java/org/java/web/girController.java
@@ -0,0 +1,19 @@
+package org.java.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:shuxiang
+ * @date:2019/8/5 {HOUR}:{MTNUTE}
+ * @Description:
+ */
+@RestController
+public class girController {
+   @GetMapping("init")
+    public String init(){
+        return "holle...";
+    }
+
+
+}
